db08346497df5ec3d5a648c12f74c4874bd95770
VIKAS A BHARADWAJ <62657642+Vikasabharadwaj@users.noreply.github.com>
2021-03-08 22:57:36 +0530

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,16 @@
+  
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $POST['email'];
+  $message = $POST['message'];
+
+  $email_from = 'vikasabharadwa@gmail.com';
+  $email_subject = 'NEW ENQUIRY';
+  $email_body = "User Name: $name. \n User Email: $visitor_email. \n User Message: $message.\n";
+  $to = "vikasabharadwa@gmail.com";
+  $headers = "From $email_from \r\n";
+  $headers .= "Reply-To $visitor_email \r\n";
+  mail($to,$email_subject,$email_body,$headers);
+  header("Location: index.html");
+  
+?>
